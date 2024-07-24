5ddc2f4e0389dfa31d1f1821ddfa92a9da094eb4
gupettine <gupettine@gmail.com>
2024-07-24 12:27:01 -0300

    feature: add zod validation

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,35 +1,61 @@
-import { Button, TextInput } from '@ignite-ui/react'
+import { Button, Text, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 
-import { Form } from '@/pages/home/components/ClaimUsernameForm/styles'
+import {
+  Form,
+  FormAnnotation,
+} from '@/pages/home/components/ClaimUsernameForm/styles'
 
 const ClaimUsernameFormSchema = z.object({
-  username: z.string(),
+  username: z
+    .string()
+    .min(3, { message: 'The user must have at least 3 letters.' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'The user can only have letters and hyphens.',
+    })
+    .transform((username) => username.toLowerCase()),
 })
 
 type ClaimUsernameFormData = z.infer<typeof ClaimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
-  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(ClaimUsernameFormSchema),
+  })
 
   async function handleClaimUsername(data: ClaimUsernameFormData) {
     console.log(data)
   }
 
   return (
-    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
-      <TextInput
-        size="sm"
-        prefix="ignite.com/"
-        placeholder="your-user"
-        {...register('username')}
-      />
-      <Button size="sm" type="submit">
-        Reserve
-        <ArrowRight />
-      </Button>
-    </Form>
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size="sm"
+          prefix="ignite.com/"
+          placeholder="your-user"
+          {...register('username')}
+        />
+        <Button size="sm" type="submit">
+          Reserve
+          <ArrowRight />
+        </Button>
+      </Form>
+
+      <FormAnnotation>
+        <Text size="sm">
+          {errors.username
+            ? errors.username.message
+            : 'Enter the desired username.'}
+        </Text>
+      </FormAnnotation>
+    </>
   )
 }
