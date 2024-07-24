387142d58b537c1845bf66b55f810f52007484aa
gupettine <gupettine@gmail.com>
2024-07-24 17:13:52 -0300

    feature: add cookies to next

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -5,6 +5,7 @@ import { ArrowRight } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
+import { AxiosError } from 'axios'
 import { api } from '@/lib/axios'
 
 import { Container, Form, FormError, Header } from '@/pages/register/styles'
@@ -49,7 +50,12 @@ export default function Register() {
         username: data.username,
       })
     } catch (err) {
-      console.log(err)
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        alert(err.response.data.message)
+        return
+      }
+
+      console.error(err)
     }
   }
 
